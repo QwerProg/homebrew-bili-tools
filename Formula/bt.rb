acb4420119ee71c8bb77b6c28d5490e4f63ca302
Chapman <touch65536@gmail.com>
2026-05-28 09:47:16 +0800

    v0.1.2

--- Formula/bt.rb
+++ Formula/bt.rb
@@ -1,8 +1,10 @@
 class Bt < Formula
   desc "B站直播开播工具 — 命令行一键开播/下播"
   homepage "https://github.com/QwerProg/bili-tools"
+  url "https://github.com/QwerProg/bili-tools/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "1ff78602f88b0dcfff4a204875cbb8c7a75ea73a7dfd873fa3131d372ecf2893"
   license "MIT"
-  head "https://github.com/QwerProg/bili-tools.git", branch: "main"
+  version "0.1.2"
 
   depends_on "rust" => :build
 
